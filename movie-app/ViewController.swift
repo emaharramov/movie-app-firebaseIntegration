9508624af6c5db34ad0d2a5782a6a8443c7a7718
emaharramov <emil.mhr@div.edu.az>
2024-11-17 15:08:11 +0400

    Initial Commit

---
+++ movie-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie-app
+//
+//  Created by Emil Maharramov on 17.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
